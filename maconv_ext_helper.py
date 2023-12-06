c6b0a8f5439853f2a2ee7352fe1baa6a8e1b920f
robin-francois <robin.francois@r2gf.com>
2023-12-06 20:50:15 +0100

    General algorithm in comments

--- maconv_ext_helper.py
+++ maconv_ext_helper.py
@@ -11,3 +11,18 @@ def parse_filename(filename):
 
 
 parse_filename("A lire absolument !!TEXT!ttxt.rsrc")
+
+# Algorithm
+
+# Part 1 - get through file tree
+
+# Part 2 - get, for each file, the type and creator
+
+# Part 3 - First, check if file matches siegfried signature and get "normal" extension
+# Part 3b - Add type,creator and extension from siegfried to database
+
+# Part 4 - If no match, search TCDB for type and creator, and existing matching extension
+
+# Part 5 - Rename file with proper extension if possible
+# Part 5b - If no extension, keep type as extension
+
